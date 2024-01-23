b8e1d6a94c3b0d89d7d460a507a6131b9d34e3aa
keefe118 <keefe19810118@gmail.com>
2024-01-23 12:25:42 +0900

    new test for AWStest01

---
+++ dbaccessmv.php
@@ -0,0 +1,48 @@
+<?php
+$dsn = 'mysql:host=database-keefe-001.crqmemsomjjy.ap-northeast-1.rds.amazonaws.com;dbname=db_services';
+$username = 'keefedb_user';
+$password = 'Phoenix1981';
+$hostname = gethostname();
+
+//try-catch
+try{
+	$pdo = new PDO($dsn,$username,$password);
+	
+	$sql = "select created_at,info from site_view_history order by created_at desc limit 1";
+
+	$stmt = $pdo->prepare($sql);
+
+	$stmt -> execute();
+
+	$rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+/*	$sqlins = "insert into site_view_history (info) values ('insert test from default ";// .  exec(hostname) . "')";
+
+	$stmtins = $pdo->prepare($sqlins);
+
+	$stmtins -> execute();
+ */
+} catch (PDOException $e) {
+}
+
+function escape1($str)
+{
+	return htmlspecialchars($str,ENT_QUOTES,'UTF-8');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>test page for database access</title>
+</head>
+<body>
+Last Access Time<br><br>
+<?php foreach ($rec as $a):?>
+	<?=eacape1($a)?><br>
+<?php endforeach; ?>
+</body>
+</html>
+
